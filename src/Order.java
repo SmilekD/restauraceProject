ba1597011f7756a57b2ee5f4212148bf06387eb5
David Smilek <smilek.david13@seznam.cz>
2024-04-03 11:14:11 +0200

    Vytvoření třídy Order

---
+++ src/Order.java
@@ -0,0 +1,67 @@
+import java.time.LocalDateTime;
+
+public class Order {
+    private int tableNumber;
+    private Dish dish;
+    private int numberOfUnits;
+    private LocalDateTime orderedTime, fulfilmentTime;
+    private boolean isPaid;
+
+    public Order(int tableNumber, Dish dish, int numberOfUnits, LocalDateTime orderedTime,
+                  LocalDateTime fulfilmentTime, boolean isPaid) {
+        this.tableNumber = tableNumber;
+        this.dish = dish;
+        this.numberOfUnits = numberOfUnits;
+        this.orderedTime = orderedTime;
+        this.fulfilmentTime = fulfilmentTime;
+        this.isPaid = isPaid;
+    }
+
+    public int getTableNumber() {
+        return tableNumber;
+    }
+
+    public void setTableNumber(int tableNumber) {
+        this.tableNumber = tableNumber;
+    }
+
+    public Dish getDish() {
+        return dish;
+    }
+
+    public void setDish(Dish dish) {
+        this.dish = dish;
+    }
+
+    public int getNumberOfUnits() {
+        return numberOfUnits;
+    }
+
+    public void setNumberOfUnits(int numberOfUnits) {
+        this.numberOfUnits = numberOfUnits;
+    }
+
+    public LocalDateTime getOrderedTime() {
+        return orderedTime;
+    }
+
+    public void setOrderedTime(LocalDateTime orderedTime) {
+        this.orderedTime = orderedTime;
+    }
+
+    public LocalDateTime getFulfilmentTime() {
+        return fulfilmentTime;
+    }
+
+    public void setFulfilmentTime(LocalDateTime fulfilmentTime) {
+        this.fulfilmentTime = fulfilmentTime;
+    }
+
+    public boolean isPaid() {
+        return isPaid;
+    }
+
+    public void setPaid(boolean paid) {
+        isPaid = paid;
+    }
+}
