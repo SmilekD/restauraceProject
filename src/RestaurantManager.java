61d37cf0870ba8a3aa62301e8c83bdc475a6a5cb
David Smilek <smilek.david13@seznam.cz>
2024-04-05 11:20:25 +0200

    Do třídy RestaurantManager přidány metody averageTime a getDishesOrderedToday

--- src/RestaurantManager.java
+++ src/RestaurantManager.java
@@ -1,3 +1,5 @@
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -20,5 +22,28 @@ public class RestaurantManager {
             System.out.println("Číslo stolu: "+order.getTableNumber()+" "+order.getDish().getTitle()+" "+order.getNumberOfUnits()+"ks.");
         }
     }
-
+    public void averageTime(List<Order> orderList){
+        long size, time = 0, minutesBetween, averageTime;
+        List<Order> finishedList = new ArrayList<>();
+        for (Order order : orderList){
+            if (order.isPaid()){
+                minutesBetween = ChronoUnit.MINUTES.between(order.getOrderedTime(), order.getFulfilmentTime());
+                time += minutesBetween;
+                finishedList.add(order);
+            }
+        }
+        size = finishedList.size();
+        averageTime = time / size;
+        System.out.println("Průměrná doba zpracování objednávky je: "+averageTime+ " minut.");
+    }
+    public void getDishesOrderedToday(List<Order> orderList){
+        LocalDateTime today = LocalDateTime.now();
+        List<Dish> dishesOrderedToday = new ArrayList<>();
+        for (Order order : orderList){
+            if (order.getOrderedTime().toLocalDate().isEqual(today.toLocalDate())){
+                Dish dish = order.getDish();
+                dishesOrderedToday.add(dish);
+            }
+        } System.out.println("Seznam jídel, která byla objednána dnes: "+dishesOrderedToday);
+    }
 }
