2d4e12ea1ff730f4e0cc4d9c79e030b6ecb0eb83
David Smilek <smilek.david13@seznam.cz>
2024-04-11 19:23:33 +0200

    discord uživatelské jméno.

--- src/Main.java
+++ src/Main.java
@@ -7,6 +7,7 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
+        //////////////// DISCORD UŽIVATELSKÉ JMÉNO: smilda.         v Engeto skupinách jako David S  :-)) /////////////////////////////////////////ú
 
         RestaurantManager restaurantManager = new RestaurantManager();
         CookBook cookBook = new CookBook();
