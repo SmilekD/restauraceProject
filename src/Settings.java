4626604ad9aed18896578ac257ecd6ffdb863eea
David Smilek <smilek.david13@seznam.cz>
2024-04-11 16:09:36 +0200

    Vytvořená třída "Settings"
    vytvořené instance s názvy a cestou textových souborů pro ukládání.

---
+++ src/Settings.java
@@ -0,0 +1,10 @@
+public class Settings {
+    private static final String FILENAMEORDERS = "resources/orders.txt";
+    public static String getFileNameOrders(){
+        return FILENAMEORDERS;
+    }
+    private static final String FILENAMEDISHES = "resources/dishes.txt";
+    public static String getFileNameDishes(){
+        return FILENAMEDISHES;
+    }
+}
